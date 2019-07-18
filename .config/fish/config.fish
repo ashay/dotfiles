4dfd6ca396de385cc7bc22d63dc112ec7a439773
Ashay Rane <ashay.rane@utexas.edu>
2019-07-17 18:09:35 -0700

    Adds functions for working with panther.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,4 +9,24 @@ function fish_right_prompt
   pwd | awk -F/ '{ print $(NF-1)"/"$NF }'
 end
 
+function ped
+  ~/src/panther/panther edit $argv
+end
+
+function pat
+  ~/src/panther/panther cat $argv
+end
+
+function cg
+  pat ~/Dropbox/coon/$argv | tr -d '\n' | pbcopy
+end
+
+function cm
+  ped ~/Dropbox/coon/$argv
+end
+
+set options (ls ~/Dropbox/coon/)
+complete --command cg --no-files --exclusive --arguments "$options"
+complete --command cm --no-files --exclusive --arguments "$options"
+
 set -g -x EDITOR nvim
